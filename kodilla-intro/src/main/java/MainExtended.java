f7be56c6b6ae3bc72957b2046d830f4b23713bb6
Aleksandra Kaczmarek <olcia78@gmail.com>
2020-05-10 01:05:23 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,13 @@
+public class MainExtended {
+    public static void main(String[] args ) {
+        String myFriend = "Aśka";
+        int herPhoneNumber = 601353535;
+        double herHeight = 1.65;
+        char herFavoriteLetter = 'H';
+        boolean isGoodDancer = (true);
+        boolean isGoodSinger = (false);
+        System.out.println(myFriend);
+        System.out.println(herPhoneNumber);
+
+    }
+}
---
+++ kodilla-intro/src/main/java/ZmiennewJavie.java
@@ -0,0 +1,13 @@
+public class ZmiennewJavie {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println(height);
+        String code1 = "Hello from FirstClass!";
+        System.out.println(code1);
+    }
+}
+
